2ce822a1bd40a51d7cfbf45ec491a7e95cb80e11
Amrutha832 <amruthatcs029@gmail.com>
2024-06-12 18:46:52 +0530

    commited the changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import { useRef } from 'react';
+
+ function App() {
+  const data=useRef(null);
+  const onHandler=e=>{
+    e.preventDefault();
+console.log(data.current.value);
+data.current.focus();
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <center>
+          <input ref={data} type="text"/>
+          <button onClick={onHandler}>submit</button>
+        </center>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
